eb8cf95dd01f9996b360ffd6385080a63d90ba6e
Moses Paul R <iammosespaulr@gmail.com>
2020-05-24 12:09:01 +0530

    mm

--- App.js
+++ App.js
@@ -15,6 +15,160 @@ import {
 import Initial from './Initial.js';
 import Stream from './Stream.js';
 
+import { AuthSession } from 'expo';
+import { AsyncStorage } from 'react-native';
+
+const REDIRECT_URL = AuthSession.getRedirectUrl();
+const scopesArr = ['user-modify-playback-state', 'user-read-currently-playing', 'user-read-playback-state', 'user-library-modify',
+  'user-library-read', 'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-public',
+  'playlist-modify-private', 'user-read-recently-played', 'user-top-read'];
+const scopes = scopesArr.join(' ');
+
+const credsB64 = btoa(`b7faffeb82e54c01bd8ff7626ff91a00:5d30aab12cbd4746aee1f173e3fbcac8`);
+const clientId = 'b7faffeb82e54c01bd8ff7626ff91a00';
+
+export async function setUserData(a, b) {
+  try {
+    await AsyncStorage.setItem(a, JSON.stringify(b));
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+export async function getUserData(a) {
+  try {
+    const value = await AsyncStorage.getItem(a);
+    if (value !== null) {
+      console.log("Retrieved Successfully")
+      console.log(value);
+    }
+    return JSON.parse(value)
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+export const getAuthorizationCode = async () => {
+  try {
+    const result = await AuthSession.startAsync({
+      authUrl:
+        'https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' +
+        clientId +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' +
+        encodeURIComponent(REDIRECT_URL),
+    })
+    return result.params.code
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+import { encode as btoa } from 'base-64';
+
+export const getTokens = async () => {
+  try {
+    const authorizationCode = await getAuthorizationCode() //we wrote this function above
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${
+        REDIRECT_URL
+        }`,
+    });
+    const responseJson = await response.json();
+    // destructure the response and rename the properties to be in camelCase to satisfy my linter ;)
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: expiresIn,
+    } = responseJson;
+
+    const expirationTime = new Date().getTime() + expiresIn * 1000;
+    await setUserData('accessToken', accessToken);
+    await setUserData('refreshToken', refreshToken);
+    await setUserData('expirationTime', expirationTime);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export const refreshTokens = async () => {
+  try {
+    const refreshToken = await getUserData('refreshToken');
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
+    });
+    const responseJson = await response.json();
+    if (responseJson.error) {
+      await getTokens();
+    } else {
+      const {
+        access_token: newAccessToken,
+        refresh_token: newRefreshToken,
+        expires_in: expiresIn,
+      } = responseJson;
+
+      const expirationTime = new Date().getTime() + expiresIn * 1000;
+      await setUserData('accessToken', newAccessToken);
+      if (newRefreshToken) {
+        await setUserData('refreshToken', newRefreshToken);
+      }
+      await setUserData('expirationTime', expirationTime);
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export async function Login() {
+  console.log(REDIRECT_URL);
+  const tokenExpirationTime = await getUserData('expirationTime');
+  console.log(tokenExpirationTime);
+  if (!tokenExpirationTime || new Date().getTime() > tokenExpirationTime) {
+    await refreshTokens();
+  } else {
+    await setUserData('accessTokenAvailable', true);
+  }
+  res = await getUserData('accessTokenAvailable')
+  res1 = await getUserData('accessToken');
+  //res3 = await getUserPlaylists();
+  
+  return (res)
+}
+
+import SpotifyWebAPI from 'spotify-web-api-js';
+
+export const getValidSPObj = async () => {
+  const tokenExpirationTime = await getUserData('expirationTime');
+  if (new Date().getTime() > tokenExpirationTime) {
+    // access token has expired, so we need to use the refresh token
+    await refreshTokens();
+  }
+  const accessToken = await getUserData('accessToken');
+  var sp = new SpotifyWebAPI();
+  await sp.setAccessToken(accessToken);
+  return sp;
+}
+
+export const getUserPlaylists = async () => {
+  const sp = await getValidSPObj();
+  const { id: userId } = await sp.getMe();
+  const { items: playlists } = await sp.getUserPlaylists(userId, { limit: 50 });
+  return playlists;
+};
+
 const App = createSwitchNavigator({
     initial: { screen: Initial },
     stream: { screen: Stream },
--- Initial.js
+++ Initial.js
@@ -7,8 +7,8 @@ import {
     TouchableHighlight,
     View
 } from 'react-native';
-import Spotify from 'rn-spotify-sdk';
 
+import {Login} from './App.js';
 
 export default class InitialScreen extends PureComponent {
     static navigationOptions = {
@@ -25,76 +25,35 @@ export default class InitialScreen extends PureComponent {
     }
 
     goToPlayer() {
-        this.props.navigation.navigate('player');
+        this.props.navigation.navigate('stream');
     }
 
-    async initializeIfNeeded() {
-        // initialize Spotify if it hasn't been initialized yet
-        if (!await Spotify.isInitializedAsync()) {
-            // initialize spotify
-            const spotifyOptions = {
-                "clientID": "<INSERT-YOUR-CLIENT-ID-HERE>",
-                "sessionUserDefaultsKey": "SpotifySession",
-                "redirectURL": "examplespotifyapp://auth",
-                "scopes": ["user-read-private", "playlist-read", "playlist-read-private", "streaming"],
-            };
-            const loggedIn = await Spotify.initialize(spotifyOptions);
-            // update UI state
-            this.setState({
-                spotifyInitialized: true
-            });
-            // handle initialization
-            if (loggedIn) {
-                this.goToPlayer();
-            }
-        } else {
-            // update UI state
-            this.setState({
-                spotifyInitialized: true
-            });
-            // handle logged in
-            if (await Spotify.isLoggedInAsync()) {
-                this.goToPlayer();
-            }
+    async spotifyLoginButtonWasPressed() {
+        res = await Login()
+        if (res == true)
+        {
+            this.goToPlayer()
         }
     }
 
-    componentDidMount() {
-        this.initializeIfNeeded().catch((error) => {
-            Alert.alert("Error", error.message);
-        });
-    }
-
-    spotifyLoginButtonWasPressed() {
-        // log into Spotify
-        Spotify.login().then((loggedIn) => {
-            if (loggedIn) {
-                // logged in
-                this.goToPlayer();
-            } else {
-                // cancelled
-            }
-        }).catch((error) => {
-            // error
-            Alert.alert("Error", error.message);
-        });
-    }
-
     render() {
-        if (!this.state.spotifyInitialized) {
-            return (<View style={styles.container}>
+        if (this.state.spotifyInitialized) {
+            return (
+            <View style={styles.container}>
                 <ActivityIndicator animating={true}
                     style={styles.loadIndicator}>
                 </ActivityIndicator>
-                <Text style={styles.loadMessage} >
-                    Loading... </Text> </View >
+                <Text style={styles.loadMessage}>
+                    Loading... 
+                </Text> 
+            </View >
             );
         } else {
             return (<View style={styles.container}>
                 <Text style={styles.greeting}>
-                    Hey!You!Log into your spotify </Text>
-                <TouchableHighlight onPress={this.spotifyLoginButtonWasPressed}
-                    style={styles.spotifyLoginButton}> <Text style={styles.spotifyLoginButtonText}> Log into Spotify </Text>
+                    Fireside Partay! </Text>
+                <TouchableHighlight onPress={this.spotifyLoginButtonWasPressed} style={styles.spotifyLoginButton}>
+                     <Text style={styles.spotifyLoginButtonText}> Log into Spotify </Text>
                 </TouchableHighlight>
             </View>
             );
@@ -123,21 +82,23 @@ const styles = StyleSheet.create({
     spotifyLoginButton: {
         justifyContent: 'center',
         borderRadius: 18,
-        backgroundColor: 'green',
+        backgroundColor: '#00FF66',
         overflow: 'hidden',
-        width: 200,
-        height: 40,
+        width: 250,
+        height: 60,
         margin: 20,
     },
     spotifyLoginButtonText: {
-        fontSize: 20,
+        fontSize: 25,
         textAlign: 'center',
-        color: 'white',
+        color: 'black',
+        fontWeight: 'bold'
     },
 
     greeting: {
-        fontSize: 20,
+        fontSize: 45,
         textAlign: 'center',
-        margin: 10,
+        color: 'green',
+        fontWeight: 'bold'
     },
 });
